3086e7d48a7827fd02e33bcadaaac33bd28b0e7e
nopslip <zacharywolff@gmail.com>
2017-04-26 16:02:00 -0600

    fixing zcash testnet privkey

--- pycoin/networks/legacy_networks.py
+++ pycoin/networks/legacy_networks.py
@@ -70,11 +70,11 @@ NETWORKS = (
     NetworkValues("Argentum", "mainnet", "ARG", b'\x97', b'\x17', b'\5', h2b("0488ADE4"), h2b("0488B21E")),
 
     # ZEC Zcash mainnet : xprv/xpub
-    NetworkValues("Zcash", "mainnet", "ZEC", b'\xef', b'\x1C\xB8',
+    NetworkValues("Zcash", "mainnet", "ZEC", b'\x80', b'\x1C\xB8',
                   b'\x1C\xBD', h2b("0488ADE4"), h2b("0488B21E")),
    
     # tZEC Zcash testnet : xpriv/xpub 
-    NetworkValues("Zcash", "testnet", "tZEC", b'\x80', b'\x1D\x25',
+    NetworkValues("Zcash", "testnet", "tZEC", b'\xEF', b'\x1D\x25',
                   b'\x1C\xBA', h2b("0488ADE4"), h2b("0488B21E")), 
 
     # BTCD BitcoinDark mainnet : xprv/xpub
